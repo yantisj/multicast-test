5e6054007473439086a830cdbac44b53941bbb5b
Jonathan Yantis <yantisj@gmail.com>
2017-07-13 08:58:38 -0400

    Initial commit

---
+++ mctest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import time
+import socket
+import sys
+import struct
+import argparse
+
+group = '232.44.44.233'
+MCAST_PORT = 7878
+mttl = 6
+
+
+def receiver(group):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', MCAST_PORT))  # use MCAST_GRP instead of '' to listen only
+                                # to MCAST_GRP, not all groups on MCAST_PORT
+    mreq = struct.pack("4sl", socket.inet_aton(group), socket.INADDR_ANY)
+
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    while True:
+        print 'test'
+        print sock.recv(16)
+
+receiver(group)
