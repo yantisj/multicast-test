9b4f881e9241f06579ea53b379729619f14e5081
Jonathan Yantis <yantisj@gmail.com>
2017-07-13 09:16:02 -0400

    Cleaned up output

--- mctest.py
+++ mctest.py
@@ -21,6 +21,7 @@ parser.add_argument("-send", metavar="string", help="Send a Message",
 parser.add_argument("-receive", help="Receive Messages from Group",
                     action="store_true")
 parser.add_argument("-group", metavar="Multicast Group (default: 232.8.8.8", type=str)
+parser.add_argument('-ttl', metavar='int', help="Multicast TTL (default 4)", type=int)
 parser.add_argument("-v", help="Verbose Output", action="store_true")
 args = parser.parse_args()
 
@@ -35,22 +36,24 @@ def receiver(group):
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     while True:
-        print ('Received: ' + sock.recv(1024))
+        print ('Received from ' + group + ': ' + sock.recv(1024))
 
 
 def sender(group):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+    sock.setsockopt(socket.IPPROTO_IP, mttl, 2)
     while 1:
         time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         mcast_msg = message + ': ' + time_now
-        print('Sending: ' + mcast_msg)
+        print('Sending to ' + group + ' (TTL ' + str(mttl) + '): ' + mcast_msg)
         sock.sendto(mcast_msg, (group, MCAST_PORT))
         time.sleep(1)
 
 if args.group:
     group = args.group
+if args.ttl:
+    mttl = args.ttl
 
 if args.send:
     message = args.send
